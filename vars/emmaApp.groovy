213e88864c8895460451b95d5891bb2711be7884
Favouredd <99230456+Favouredd@users.noreply.github.com>
2022-06-12 21:55:43 -0400

    added emmaApp (#4)

---
+++ vars/emmaApp.groovy
@@ -0,0 +1,34 @@
+def call(String repoUrl){
+  pipeline {
+    agent any
+    stages {
+        stage ('1-Git-Clone') {
+            steps {
+                checkout([$class: 'GitSCM', branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[credentialsId: 'GitHubCredentials', url: 'https://github.com/etechapp/parallel-mulitbranch-pipeline.git']]])
+            } 
+        }
+        stage ('2-Parallel-Jobs') {
+            parallel {
+                stage ('sub-job-1') {
+                    steps {
+                        sh 'lscpu'
+                        sh 'uptime'
+                    }
+                }
+                stage ('sub-job-2') {
+                    steps {
+                        sh 'pwd'
+                        sh 'ps -ef'
+                    }
+                }
+                stage ('sub-job-3'){
+                    steps{
+                        sh 'free -g'
+                        sh 'bash /var/lib/jenkins/workspace/parallel-pipeline/os-jenkins-check.sh'
+                    }
+                }
+            }
+        }
+    }
+}
+ }
